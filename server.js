ef7f7dce56b8b7f0523a02be91c306e24fc3df37
Adith Kumar <adithkumar.ba@gmail.com>
2018-11-16 17:21:19 +0530

    Added login

--- server.js
+++ server.js
@@ -2,12 +2,32 @@ var mysql = require('mysql');
 const express = require("express");
 const app = express();
 const bodyparser = require('body-parser');
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
+
+var session  = require('express-session');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser()); // read cookies (needed for auth)
+require('./rename/passport')(passport); // pass passport for configuration
+app.use(session({
+	secret: 'boomboomboom',
+	resave: true,
+	saveUninitialized: true
+ } )); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 
 app.set('view engine','ejs');
 app.use('/',express.static('public'));
 
-var urlencodedParser = bodyparser.urlencoded({ extended: false });
+app.use(bodyparser.urlencoded({
+	extended: true
+}));
+//var urlencodedParser = bodyparser.urlencoded({ extended: false });
+app.use(bodyparser.json());
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -24,7 +44,7 @@ con.connect(function(err) {
   }
   });
 
-app.get ('/city/:city',function(req , res) {
+app.get ('/city/:city', function(req , res) {
 
     con.query( `SELECT e.eid,e.ename,e.venue, e.edate FROM eventlist AS e,location AS l where e.eid=l.eid and l.city='${req.params.city}'`, function(err,rows,fields) {
       if(err)
@@ -47,7 +67,7 @@ app.get('/category/:category', function(req, res){
   })
 } )
 
-app.get('/event',function(req , res){
+app.get('/event', function(req , res){
   con.query(`SELECT eid,ename,venue,edate from eventlist order by edate limit 5`,function(err,rows,fields){
     if(err)
       console.log(err);
@@ -57,8 +77,13 @@ app.get('/event',function(req , res){
     }
   });
 })
-app.post('/event', urlencodedParser , function(req , res ){
-      con.query(`insert into eventlist(ename,venue,cid,edate,oid) values(?,?,?,?,?)`,[ req.body.ename ,req.body.venue, req.body.cid, req.body.edate , req.body.oid ], function(err,rows,fields){
+
+app.get('/event/add', isLoggedIn, function(req, res){
+	 res.render('add');
+})
+
+ app.post('/event/add' , function(req , res ){
+    /*  con.query(`insert into eventlist(ename,venue,cid,edate,oid) values(?,?,?,?,?)`,[ req.body.ename ,req.body.venue, req.body.cid, req.body.edate , req.body.oid ], function(err,rows,fields){
         if(err)
           console.log(err);
         else
@@ -68,13 +93,20 @@ app.post('/event', urlencodedParser , function(req , res ){
     con.query(`select eid from eventlist where ename="${req.body.ename}"`, function(err,rows,fields) {
         con.query(`insert into location values(?,?)`, [ rows[0].eid , req.body.city]);
         con.query(`insert into eventdisc(eid,disc,ename,venue) values(?,?,?,?)`,[rows[0].eid, req.body.comment , req.body.ename, req.body.venue]);
-    });
+    }); */
+    con.query(`CALL tresin(?,?,?,?,?,?,?)`,[ req.body.ename ,req.body.venue, req.body.cid , req.body.oid, req.body.edate , req.body.city , req.body.comment], function(err,rows){
+      if(err)
+        console.log(err);
+      else {
+          console.log('succ insert');
+        }
+    })
 
 //     console.log(req.body);
-      res.redirect('back');
+      res.redirect('/event');
 
 })
-app.get('/event/eventdisc/:eventid',function(req,res){
+app.get('/event/eventdisc/:eventid', function(req,res){
   con.query(`select e.ename,e.venue,e.disc,d.edate from eventdisc e , eventlist d where e.eid=d.eid and e.eid=${req.params.eventid}`,function(err,rows,fields){
     if(err)
       console.log(err);
@@ -84,7 +116,7 @@ app.get('/event/eventdisc/:eventid',function(req,res){
     }
   })
 })
-app.get('/event/edit/:eid',function(req,res){
+app.get('/event/edit/:eid', isLoggedIn ,function(req,res){
   con.query(`select e.eid,e.ename,e.venue,e.disc,d.edate from eventdisc e , eventlist d where e.eid=d.eid and e.eid=${req.params.eid}`,function(err,rows,fields){
     if(err)
       console.log(err);
@@ -96,7 +128,7 @@ app.get('/event/edit/:eid',function(req,res){
 
 })
 app.use(methodOverride('_method'));
-app.put('/event/edit', urlencodedParser , function (req , res) {
+app.put('/event/edit', function (req , res) {
    con.query(`update eventlist set ename="${req.body.ename}",edate="${req.body.edate}",cid="${req.body.cid}",oid="${req.body.oid}",venue="${req.body.venue}" where eid=?`,[req.query.person], function(err,fields){
     if(err)
       console.log(err);
@@ -117,7 +149,7 @@ app.put('/event/edit', urlencodedParser , function (req , res) {
   //console.log(req.body);
 })
 
-app.get('/event/deletion', function(req,res){
+app.get('/event/deletion', isLoggedIn , function(req,res){
   con.query(`SELECT eid,ename,venue,edate from eventlist order by edate`,function(err,rows,fields){
     if(err)
       console.log(err);
@@ -139,6 +171,61 @@ app.delete('/resource', function(req , res){
  //  console.log(req.query.person);
   res.redirect('/event/deletion');
 })
+
+
+app.get('/login', function(req, res) {
+
+		// render the page and pass in any flash data if it exists
+		res.render('login', { message: req.flash('loginMessage') });
+	});
+
+	// process the login form
+	app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/event', // redirect to the secure profile section
+            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+		}),
+        function(req, res) {
+            console.log("hello");
+
+            if (req.body.remember) {
+              req.session.cookie.maxage = 1000 * 60 * 3;
+            } else {
+              req.session.cookie.expires = false;
+            }
+        res.redirect('/event');
+    });
+
+	// =====================================
+	// SIGNUP ==============================
+	// =====================================
+	// show the signup form
+	app.get('/signup', function(req, res) {
+		// render the page and pass in any flash data if it exists
+		res.render('signup', { message: req.flash('signupMessage') });
+	});
+
+	// process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/login', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+  app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/event');
+	});
+  function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/login');
+}
+
+
 
 app.listen(3000, () => {
   console.log("Server running up on port 3000");
